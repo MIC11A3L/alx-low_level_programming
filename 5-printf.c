e08f58b252446189c636b9c7f389ab1d1f4630dc
MIC11A3L <michaelukachukwu@yahoo.com>
2022-03-10 09:28:36 -0800

    C task

---
+++ 5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ *main -Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a part,\n");
+	return (0);
+}
