fe16f7b35e3b0a364739d44cb094572717c5128b
MIC11A3L <99387901+MIC11A3L@users.noreply.github.com>
2022-03-11 21:39:16 +0100

    C variable scripts

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
-#include
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit + '0');
+
+	putchar('\n');
+
+	return (0);
+}
