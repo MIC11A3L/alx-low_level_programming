948faddeceb79cd09c9ea72618e38fb6bc1d5358
MIC11A3L <99387901+MIC11A3L@users.noreply.github.com>
2022-03-16 19:26:24 +0100

    debuggin scripts

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,71 +1,22 @@
-nclude <stdio.h>
-
-#include "main.h"
-
+#include <stdio.h>
 /**
- *
- *   * print_remaining_days - takes a date and prints how many days are
- *
- *     * left in the year, taking leap years into account
- *
- *       * @month: month in number format
- *
- *         * @day: day of month
- *
- *           * @year: year
- *
- *             * Return: void
- *
- *               */
-
-void print_remaining_days(int month, int day, int year)
-
+  * main - causes an infinite loop
+  * Return: 0
+  */
+int main(void)
 {
+	int i;
 
-		if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+	printf("Infinite loop incoming :(\n");
 
-				{
+	i = 0;
 
-							if (month > 2 && day >= 60)
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
-										{
+	printf("Infinite loop avoided! \\o/\n");
 
-														day++;
-
-																}
-
-
-
-									printf("Day of the year: %d\n", day);
-
-											printf("Remaining days: %d\n", 366 - day);
-
-
-
-												}
-
-			else
-
-					{
-
-								if (month == 2 && day == 60)
-
-											{
-
-															printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-
-																	}
-
-										else
-
-													{
-
-																	printf("Day of the year: %d\n", day);
-
-																				printf("Remaining days: %d\n", 365 - day);
-
-																						}
-
-											}
-
+	return (0);
 }
