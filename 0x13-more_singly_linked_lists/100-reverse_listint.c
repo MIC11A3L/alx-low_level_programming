706bb3da2f7ed7c560c4be93dd89d531584188fe
MIC11A3L <99387901+MIC11A3L@users.noreply.github.com>
2022-04-26 16:14:50 +0100

    11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,26 @@
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * reverse_listint - define function
- * @head: describe argument
- * Return: what does it return?
- */
+ * reverse_listint - function to reverse lists
+ * @head: Pointer to a pointer pointing to a struct
+ *
+ * Return: Returns a pointer to the first node of the reversed list
+**/
+
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+listint_t *tmp = *head;
+listint_t *prev = NULL, *next = NULL;
+
+while (tmp != NULL)
+{
+next = tmp->next;
+tmp->next = prev;
+prev = tmp;
+tmp = next;
+}
+*head = prev;
+
+return (*head);
 }
